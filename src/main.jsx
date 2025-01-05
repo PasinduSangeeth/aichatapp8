de9f655bbf9a94bea506a532ffd7b4a0e92f1e9a
Pasindu <pasindusangeeth968@gmail.com>
2025-01-05 07:47:54 +0530

    homepage and dashboard path defined

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
+import { Children, StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element:( <Homepage />),
+  },
+  {
+    path: "/",
+    element:<DashboardPage />,
+    Children:[
+      {path:"/dashboard/chats/:id",element:<ChatPage />}
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
